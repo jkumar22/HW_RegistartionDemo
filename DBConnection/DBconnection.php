4c7a2c65ab014e1a7783ed7c3765eaaf19d96af8
Jay Patel <jnp234@gmail.com>
2018-06-13 23:26:13 -0400

    Initial commit

---
+++ DBConnection/DBconnection.php
@@ -0,0 +1,7 @@
+<?php
+    $dbCon = mysqli_connect("localhost", "id6160782_jnpatel", "password", "id6160782_hwbook");
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect: " . mysqli_connect_error();
+    }
+?>
